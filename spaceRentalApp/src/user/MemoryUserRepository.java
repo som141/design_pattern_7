e37f1b913ba8db60f31c330bde0b8b218b564600
김형균 <32211307@dankook.ac.kr>
2025-11-02 00:56:38 +0900

    user구현, .idea/,.xml 파일 제외시킴

--- spaceRentalApp/src/user/MemoryUserRepository.java
+++ spaceRentalApp/src/user/MemoryUserRepository.java
@@ -1,5 +1,6 @@
 package user;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -10,9 +11,27 @@ import java.util.concurrent.ConcurrentHashMap;
 public class MemoryUserRepository {
     private static final Map<Long, User> store = new ConcurrentHashMap<>();
 
-    public void save(User user) { /* 사용자 저장 로직 */ }
-    public User findById(Long id) { /* ID로 사용자 조회 로직 */ return null; }
-    public List<User> findAll() { /* 모든 사용자 조회 로직 */ return null; }
-    public void deleteById(Long id) { /* ID로 사용자 삭제 로직 */ }
-    public boolean updateById(Long id, User updateUser) { /* 사용자 정보 업데이트 로직 */ return false; }
+    public void save(User user) {       // 사용자 저장 로직
+        store.put(user.getUserId(), user);
+    }
+
+    public User findById(Long id) {     // ID로 사용자 조회 로직
+        return store.get(id);
+    }
+
+    public List<User> findAll() {       // 모든 사용자 조회 로직
+        return new ArrayList<>(store.values());
+    }
+
+    public void deleteById(Long id) {   // ID로 사용자 삭제 로직
+        store.remove(id);
+    }
+
+    public boolean updateById(Long id, User updateUser) {       // 사용자 정보 업데이트 로직
+        if(store.containsKey(id)){      // 해당 사용자 존재하는 경우
+            store.put(id, updateUser);
+            return true;
+        }
+        return false;
+    }
 }
